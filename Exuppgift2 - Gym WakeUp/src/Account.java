13a808a14d5d2fce2c5e134275e0be9f7af7e739
goodsys-se <robert@goodsystems.se>
2020-12-07 18:32:43 +0100

    Monday coding session..

--- Exuppgift2 - Gym WakeUp/src/Account.java
+++ Exuppgift2 - Gym WakeUp/src/Account.java
@@ -1,7 +1,168 @@
+import java.util.Scanner;
+
 public class Account {
-    private String accountName;
     private String accountPassword;
     private String personNummer;
+    private boolean status;
+    private int membershipPrice;
+    private int membershipLenght;
+
+
+    public void bliMedlem() {
+        Scanner userInput = new Scanner(System.in);
+        String personnummerAttempt;
+        boolean personnummerControl;
+
+        if (this.getStatus()) {
+            System.out.println("Du är redan medlem");
+        } else {
+            do {
+                System.out.println("Please enter your Personnummer in the following format YYMMDD-XXXX: ");
+                personnummerAttempt = userInput.nextLine();
+                personnummerControl = luhnAlgorithm(personnummerAttempt);
+                if (personnummerControl == false) {
+                    System.out.println("Incorrect personnummer, please try again");
+                } else {
+                    this.setPersonNummer(personnummerAttempt);
+                }
+            }
+            while (personnummerControl == false);
+            System.out.println("Please choose a password: ");
+            this.setAccountPassword(userInput.next());
+
+            System.out.println("");
+            System.out.println("PRISLISTA:");
+            System.out.println("Medlemskap - 100 SEK");
+            System.out.println("1-2 månader – 400 SEK/månad");
+            System.out.println("3-6 månader – 350 SEK/månad");
+            System.out.println("7-12 månader – 300 SEK/månad");
+            System.out.println("Längre än 12 månader – 250 SEK/månad");
+
+            System.out.println("");
+
+            System.out.println("Hur många månader vill du vara medlem: ");
+            this.membershipLenght = userInput.nextInt();
+            this.membershipPrice = calcMembership(this.membershipLenght);
+            System.out.print("Totalt att betala: ");  // FIXA!
+            System.out.print(this.membershipPrice);
+            System.out.println(" SEK");
+            System.out.println("Välkommen som medlem!");
+        }
+
+    }
+
+    public int calcMembership(int months) {
+        int price;
+        int totalprice;
+        int baseMembership = 100;
+        if (months >= 1 && months <= 2) {
+            price = 400;
+        } else {
+            if (months <= 6) {
+                price = 350;
+            } else {
+                if (months > 12) {
+                    price = 250;
+                } else {
+                    price = 300;
+                }
+            }
+        }
+        totalprice = price * months + baseMembership;
+        return totalprice;
+    }
+
+    public void loggaIn() {
+        Scanner userInput = new Scanner(System.in);
+        String personnummerAttempt;
+        String passwordAttempt;
+
+        if (this.getStatus()) {
+            System.out.println("Du är redan inloggad");
+        } else {
+
+            System.out.println("Please login");
+            System.out.println("Personnummer: ");
+            personnummerAttempt = userInput.next();
+            System.out.println("Password: ");
+            passwordAttempt = userInput.next();
+            if (personnummerAttempt.equals(this.getPersonNummer()) && (passwordAttempt.equals(this.getAccountPassword()))) {
+                this.setStatus(true);
+                System.out.println("Welcome!");
+            } else {
+                System.out.println("Fel inloggning!");
+            }
+        }
+    }
+
+    public void bokaPass() {
+        if (this.getStatus()) {
+            System.out.println("Boka pass här..");
+
+
+        } else {
+            System.out.println("Du måste logga in först");
+        }
+    }
+
+    public static boolean luhnAlgorithm(String persnummer) {
+        boolean result; // result variable gets created, this will later be returned after the method finishes
+
+        //if-statement that ensures that the length is fitting, if not, returns false and ends the method
+        if (persnummer.length() != 11){
+            result = false;
+            return result;
+        }
+        char[] persnummertoChar = removeCharAt(persnummer).toCharArray();// Removing the hyphen or "+" from the String personnummer with the method removeCharAt and converting the result into a charArray
+        int[] persnummerToInt = new int[persnummertoChar.length]; // initializing an intArray that will be used to multiply the values later on. Due to the use of charArray all numbers from the personnummer will be separate values in the array
+
+        //for-loop to enter the charArray values into the intArray
+        for (int i = 0; i < persnummerToInt.length; i++) {
+            persnummerToInt[i] = (int) persnummertoChar[i] - 48;
+            // persnummertoChar.getNumericValue(i);
+        }
+        int tmp, n = 0;
+        int sum = 0;
+        //for-loop to multiply every other value of the intArray with 2 and every other value with 1
+        for (int i = 0; i < 10; i++) {
+            if ((i % 2) == 0) {
+                tmp = persnummerToInt[i] * 2;
+            } else {
+                tmp = persnummerToInt[i] * 1;
+            }
+            while (tmp > 0) {     // Loop för att ta ut digit sum av talet
+                n = tmp % 10;
+                sum = sum + n;
+                tmp = tmp / 10;
+            }
+        }
+        sum = sum / 10; //Sum gets divided by 10 and stored in the variable
+
+        /*if-statment to double check that the value is not zero, as this would make result being true,
+        the user could for example enter 000000-0000 and get a true result without below if-statment*/
+        if (sum == 0){
+            result = false;
+            return result;
+        }
+        // Nu dags att kolla ifall allt stämmer och att personnumret har rätt summa och är korrekt
+        if (sum == Math.floor(sum)) {
+            result = true;
+        } else result = false;
+        return result;
+    }
+
+    private static String removeCharAt(String s) {
+        return s.substring(0, 6) + s.substring(6 + 1);
+    }
+
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
     public String getPersonNummer() {
         return personNummer;
@@ -11,13 +172,6 @@ public class Account {
         this.personNummer = personNummer;
     }
 
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
 
     public void setAccountPassword(String accountPassword) {
         this.accountPassword = accountPassword;
--- Exuppgift2 - Gym WakeUp/src/Main.java
+++ Exuppgift2 - Gym WakeUp/src/Main.java
@@ -6,98 +6,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         Account account1 = new Account();
-        String usernameAttempt;
-        String passwordAttempt;
-        String personnummerAttempt;
-        boolean personnummerControl;
-
-
+        int menyVal;
 
         do {
-            System.out.println("Please enter your Personnummer in the following format YYMMDD-XXXX: ");
-            personnummerAttempt = userInput.nextLine();
-            personnummerControl = luhnAlgorithm(personnummerAttempt);
-            if (personnummerControl == false){
-                System.out.println("Incorrect personnummer, please try again");
+            printMeny(account1.getStatus());
+            menyVal = userInput.nextInt();
+            userInput.nextLine();
+            switch(menyVal) {
+                case 1:
+                    account1.loggaIn();
+                    break;
+                case 2:
+                    account1.bliMedlem();
+                    break;
+                case 3:
+                    account1.bokaPass();
+                    break;
+                case 4:
+                    System.out.println("Välkommen åter.");
+                    break;
+                default:
+                    System.out.println("Fel menyval");
+                    // code block
             }
         }
-        while (personnummerControl == false);
-
-            System.out.println("Please choose a username: ");
-            account1.setAccountName(userInput.nextLine());
-
-            System.out.println("Please choose a password: ");
-            account1.setAccountPassword(userInput.next());
-
-
-            System.out.println("Please login");
-            System.out.println("Username: ");
-            usernameAttempt = userInput.next();
-            if (usernameAttempt.toLowerCase().equals(account1.getAccountName().toLowerCase())) {
-                System.out.println("Password: ");
-                passwordAttempt = userInput.next();
-                if (passwordAttempt.equals(account1.getAccountPassword())) {
-                    System.out.println("Welcome " + account1.getAccountName() + "!");
-                }
-            }
-        }
-
-    public static boolean luhnAlgorithm(String persnummer) {
-        boolean result; // result variable gets created, this will later be returned after the method finishes
-
-        //if-statement that ensures that the length is fitting, if not, returns false and ends the method
-        if (persnummer.length() != 11){
-            result = false;
-            return result;
-        }
-        char[] persnummertoChar = removeCharAt(persnummer).toCharArray();// Removing the hyphen or "+" from the String personnummer with the method removeCharAt and converting the result into a charArray
-        int[] persnummerToInt = new int[persnummertoChar.length]; // initializing an intArray that will be used to multiply the values later on. Due to the use of charArray all numbers from the personnummer will be separate values in the array
-
-        //for-loop to enter the charArray values into the intArray
-        for (int i = 0; i < persnummerToInt.length; i++) {
-            persnummerToInt[i] = (int) persnummertoChar[i] - 48;
-        }
-
-        //for-loop to multiply every other value of the intArray with 2 and every other value with 1
-        for (int i = 0; i < 9; i++) {
-            if ((i % 2) == 0) {
-                persnummerToInt[i] = persnummerToInt[i] * 2;
-            } //else persnummerToInt[i] = persnummerToInt[i] * 1; // not necessary to multiply with 1?
-        }
-        //Returning the multiplied intArray into a String, this needs to be done so we can later on split all double-digit values into separate values (example splitting 10 into 1 and 0)
-        persnummer = String.join("", IntStream.of(persnummerToInt).mapToObj(String::valueOf).toArray(String[]::new)); //
-
-        //storing the String value of the personnummer in a charArray, this splits all double-digit values into two separate values
-        persnummertoChar = persnummer.toCharArray();
-
-        //new intArray needs to be created to fit the length of the new values, as it may be to long to fit into the old array
-        int[] control = new int[persnummertoChar.length];
-
-        //for-loop to enter the charArray values into the intArray
-        for (int i = 0; i < control.length; i++) {
-            control[i] = (int) persnummertoChar[i] - 48;
-        }
-         double sum = 0; // sum variable initialized
-        // for-loop used to sum up all the values in the newly created intArray (variable "control")
-        for (int i : control) {
-            sum += i;
-        }
-         sum = sum / 10; //Sum gets divided by 10 and stored in the variable
-
-        /*if-statment to double check that the value is not zero, as this would make result being true,
-        the user could for example enter 000000-0000 and get a true result without below if-statment*/
-        if (sum == 0){
-            result = false;
-            return result;
-        }
-        if (sum == Math.floor(sum)) {
-            result = true;
-        } else result = false;
-
-        return result;
+        while (menyVal != 4);
     }
 
-    private static String removeCharAt(String s) {
-        return s.substring(0, 6) + s.substring(6 + 1);
+    public static void printMeny(boolean status) {
+        if (status) {
+            System.out.println("3. Boka pass");
+            System.out.println("4. Avsluta");
+        } else {
+            System.out.println("1. Logga in");
+            System.out.println("2. Bli medlem");
+            System.out.println("4. Avsluta");
+        }
+
     }
+
+
 }
